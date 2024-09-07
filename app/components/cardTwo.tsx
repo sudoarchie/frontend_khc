109d6a28273d4e02d72fa039ad7f9dab38321ffb
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-09-07 18:00:13 +0530

    fix bug in size

--- app/components/cardTwo.tsx
+++ app/components/cardTwo.tsx
@@ -11,10 +11,10 @@ interface cardData {
 export function CardTwo({ title, discription, image, url }: cardData) {
   return (
     <Link
-      className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg focus:outline-none focus:shadow-lg transition "
+      className="flex flex-col w-[300px] group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg focus:outline-none focus:shadow-lg transition "
       href={url}
     >
-      <div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden w-[300px]">
+      <div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden ">
         <Image
           className="size-full absolute top-0 start-0 object-cover group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl h-[200px]"
           src={image || img1}
@@ -23,7 +23,13 @@ export function CardTwo({ title, discription, image, url }: cardData) {
       </div>
       <div className="p-4 md:p-5">
         <h3 className="text-lg font-bold text-gray-800 ">{title}</h3>
-        <p className="mt-1 text-gray-500 ">{discription}</p>
+        {discription ? (
+          <p className="mt-1 text-gray-500 ">
+            {discription.split(" ").slice(0, 5).join(" ")}...
+          </p>
+        ) : (
+          <></>
+        )}
       </div>
     </Link>
   );
