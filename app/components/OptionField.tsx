bf1b1a21edb0b013fa30d43cc049e1fff07a6987
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-09-12 18:27:28 +0530

    option field component forward ref

--- app/components/OptionField.tsx
+++ app/components/OptionField.tsx
@@ -1,36 +1,49 @@
-interface option {
+import React, { forwardRef, SelectHTMLAttributes } from "react";
+
+interface OptionFieldProps extends SelectHTMLAttributes<HTMLSelectElement> {
   Label: string;
   Option: string[];
   className?: string;
 }
-export default function OptionField({ Label, Option, className }: option) {
-  return (
-    <div className={`w-full ${className}`}>
-      <label className="block mb-1 text-sm text-slate-800">{Label}</label>
 
-      <div className="relative">
-        <select className="w-full h-10 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer">
-          {Option.map((info, index) => (
-            <option value={info} key={index}>
-              {info}
-            </option>
-          ))}
-        </select>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.2"
-          stroke="currentColor"
-          className="h-5 w-5 ml-1 absolute top-2.5 right-2.5 text-slate-700"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
-          />
-        </svg>
+const OptionField = forwardRef<HTMLSelectElement, OptionFieldProps>(
+  ({ Label, Option, className, ...props }, ref) => {
+    return (
+      <div className={`w-full ${className}`}>
+        <label className="block mb-1 text-sm text-slate-800">{Label}</label>
+
+        <div className="relative">
+          <select
+            ref={ref}
+            className="w-full h-10 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer"
+            {...props}
+          >
+            {Option.map((info, index) => (
+              <option value={info} key={index}>
+                {info}
+              </option>
+            ))}
+          </select>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.2"
+            stroke="currentColor"
+            className="h-5 w-5 ml-1 absolute top-2.5 right-2.5 text-slate-700"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
+            />
+          </svg>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
+
+OptionField.displayName = "OptionField";
+
+export default OptionField;
