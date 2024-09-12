181f0fb82a9bae1bb8679647b38ee2c7e5cdcf36
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-09-12 13:12:40 +0530

    ✨ New feature tanstack query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { Provider } from "@/utils/Provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Provider>{children}</Provider>
+      </body>
     </html>
   );
 }
