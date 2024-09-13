e8ecc1ade27e354738d46f271e5d1687b4a6cba5
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-09-13 19:08:18 +0530

    react hook form in categoury management

--- app/admin/dashboard/management/categorymanagement/page.tsx
+++ app/admin/dashboard/management/categorymanagement/page.tsx
@@ -1,8 +1,15 @@
+"use client";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { Box } from "@/app/components/Box";
 import { InputField } from "@/app/components/Inputfield";
 import OptionField from "@/app/components/OptionField";
 import { PrimaryButton } from "@/app/components/PrimaryButton";
 import TableField from "@/app/components/TableField";
+
+type Inputs = {
+  categoury: string;
+  material: string;
+};
 
 export default function CategoryManagement() {
   const CategoryList = ["cbse", "igsc", "Upboard"];
@@ -14,6 +21,14 @@ export default function CategoryManagement() {
       EditLink: "https://edilitlink.com",
     },
   ];
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <div className="flex flex-col w-[95%] m-5 mx-auto">
       <Box
@@ -21,21 +36,26 @@ export default function CategoryManagement() {
         SubHeading="Choose category and enter category name"
         className="w-full "
       >
-        <OptionField
-          Label={"Choose Category"}
-          Option={CategoryList}
-          className=" mx-auto mt-5"
-        ></OptionField>
-        <InputField
-          label={"Enter material "}
-          placeholder={""}
-          className=" mx-auto mt-5"
-          type={""}
-        ></InputField>
-        <PrimaryButton
-          Name={"+ ADD SUBJECT"}
-          className="w-full mx-auto mt-5"
-        ></PrimaryButton>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <OptionField
+            Label={"Choose Category"}
+            Option={CategoryList}
+            className=" mx-auto mt-5"
+            {...register("categoury", { required: true })}
+          ></OptionField>
+          <InputField
+            label={"Enter material "}
+            placeholder={""}
+            className=" mx-auto mt-5"
+            type={""}
+            {...register("material", { required: true })}
+          ></InputField>
+          <PrimaryButton
+            Name={"+ ADD SUBJECT"}
+            className="w-full mx-auto mt-5"
+            type="submit"
+          ></PrimaryButton>
+        </form>
       </Box>
       <TableField
         rows={data}
