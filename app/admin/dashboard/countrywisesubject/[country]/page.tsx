993980e322b2c9cd95a50f67cb8b0fa3dfce314d
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-09-13 19:22:43 +0530

    react hook form country wise subject

--- app/admin/dashboard/countrywisesubject/[country]/page.tsx
+++ app/admin/dashboard/countrywisesubject/[country]/page.tsx
@@ -1,8 +1,13 @@
+"use client";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { Box } from "@/app/components/Box";
 import { InputField } from "@/app/components/Inputfield";
 import { PrimaryButton } from "@/app/components/PrimaryButton";
 import TableField from "@/app/components/TableField";
 
+type Inputs = {
+  subject: string;
+};
 export default function Country({ params }: { params: { country: string } }) {
   const subjectList = [
     {
@@ -18,19 +23,31 @@ export default function Country({ params }: { params: { country: string } }) {
       Subject: "Computer science",
     },
   ];
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <div>
       <Box Heading="ADD SUBJECT" className="w-[95%] mx-auto">
-        <InputField
-          label={"Subject Name"}
-          placeholder={""}
-          type={"text"}
-          className="w-[95%] mx-auto mt-5"
-        ></InputField>
-        <PrimaryButton
-          Name={"+ ADD SUBJECT"}
-          className="w-[95%] mx-auto mt-5"
-        ></PrimaryButton>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <InputField
+            label={"Subject Name"}
+            placeholder={""}
+            type={"text"}
+            className="w-[95%] mx-auto mt-5"
+            {...register("subject", { required: true })}
+          ></InputField>
+          <PrimaryButton
+            Name={"+ ADD SUBJECT"}
+            className="w-[95%] mx-auto mt-5"
+            type="submit"
+          ></PrimaryButton>
+        </form>
       </Box>
       <Box className="w-[95%] mx-auto">
         <TableField rows={subjectList}></TableField>
