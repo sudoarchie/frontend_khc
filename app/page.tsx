a60a74bd8f5c3e0d62e84290f57d268f64b0ce6b
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-09-14 17:33:07 +0530

    Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import { Navbar } from "./components/Navbar";
-
+import img1 from "@/public/download.png";
 export default function Home() {
   return (
     <div>
       <Navbar></Navbar>
+      <div className="bg-[#272F42] h-[500px]">
+        <div className="flex justify-between items-center">
+          <div className="w-1/2">
+            <h1 className="text-4xl font-bold text-white">Welcome to KHC</h1>
+          </div>
+          <Image src={img1} alt={"Khc Image"} className="h-[500px]"></Image>
+        </div>
+      </div>
     </div>
   );
 }
