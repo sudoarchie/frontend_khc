22f4a4f4c3125d33a5df0f30ab20354517121bfb
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-09-22 00:15:57 +0530

    ✨ New feature videos

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -1,11 +1,59 @@
 import { VideoCard } from "../components/videoCard";
 
 export default function Videos() {
+  const data = [
+    {
+      title: "How To Build Anything | Art of Reverse Engineering",
+      href: "https://youtu.be/EiOvxYja8GU",
+    },
+    {
+      title: "How To Build Anything | Art of Reverse Engineering",
+      href: "https://youtu.be/EiOvxYja8GU",
+    },
+    {
+      title: "How To Build Anything | Art of Reverse Engineering",
+      href: "https://youtu.be/EiOvxYja8GU",
+    },
+    {
+      title: "How To Build Anything | Art of Reverse Engineering",
+      href: "https://youtu.be/EiOvxYja8GU",
+    },
+    {
+      title: "How To Build Anything | Art of Reverse Engineering",
+      href: "https://youtu.be/EiOvxYja8GU",
+    },
+    {
+      title: "How To Build Anything | Art of Reverse Engineering",
+      href: "https://youtu.be/EiOvxYja8GU",
+    },
+    {
+      title: "How To Build Anything | Art of Reverse Engineering",
+      href: "https://youtu.be/EiOvxYja8GU",
+    },
+    {
+      title: "How To Build Anything | Art of Reverse Engineering",
+      href: "https://youtu.be/EiOvxYja8GU",
+    },
+    {
+      title: "How To Build Anything | Art of Reverse Engineering",
+      href: "https://youtu.be/EiOvxYja8GU",
+    },
+    {
+      title: "How To Build Anything | Art of Reverse Engineering",
+      href: "https://youtu.be/EiOvxYja8GU",
+    },
+    {
+      title: "How To Build Anything | Art of Reverse Engineering",
+      href: "https://youtu.be/EiOvxYja8GU",
+    },
+  ];
   return (
     <div>
-      <h1 className="text-center text-4xl my-5">More Videos</h1>
-      <div className="flex flex-wrap gap-5">
-        <VideoCard></VideoCard>
+      <h1 className="text-center font-bold text-4xl my-5">More Videos</h1>
+      <div className="flex flex-wrap gap-5 justify-center w-[90%] mx-auto">
+        {data.map((info, index) => (
+          <VideoCard title={info.title} href={info.href}></VideoCard>
+        ))}
       </div>
     </div>
   );
