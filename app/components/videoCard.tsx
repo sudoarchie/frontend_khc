e1f95bfa29fa0239bb6ce84008139d3ecad6d0a4
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-09-23 20:35:00 +0530

    modified video card

--- app/components/videoCard.tsx
+++ app/components/videoCard.tsx
@@ -3,7 +3,7 @@ import img from "@/public/Untitled design.png";
 import Link from "next/link";
 import GradientCard from "./gradientCard";
 interface video {
-  image?: StaticImageData;
+  image?: StaticImageData | string;
   title: string;
   href?: string;
 }
@@ -17,6 +17,9 @@ export function VideoCard({ image, title, href }: video) {
         <Image
           src={image || img}
           alt="thumbnail"
+          width={0}
+          height={0}
+          sizes="100vw"
           className="group-hover:scale-95 transition-transform duration-200 w-[95%] mx-auto mt-2 rounded-2xl"
         />
 
