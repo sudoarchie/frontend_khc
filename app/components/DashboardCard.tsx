3ea3b03cea40c91d562a1d862e2924b1e309e682
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-09-23 20:41:14 +0530

    dashboard card change

--- app/components/DashboardCard.tsx
+++ app/components/DashboardCard.tsx
@@ -1,12 +1,26 @@
+import Image, { StaticImageData } from "next/image";
+import GradientCard from "./gradientCard";
+import img from "@/public/Frame 97 (1).png";
+
 interface DashboardInterface {
   title: string;
   dataNumber: number;
+  image?: StaticImageData;
 }
-export function DashboardCard({ title, dataNumber }: DashboardInterface) {
+export function DashboardCard({
+  title,
+  dataNumber,
+  image,
+}: DashboardInterface) {
   return (
-    <div className="w-full flex flex-col justify-center items-center saspect-[3/1] aspect-square  max-w-[230px] rounded-2xl border-2 border-borderColor p-4">
-      <h1 className="font-bold text-2xl">{dataNumber}</h1>
-      <h2 className="text-gray-500 relative top-5">{title}</h2>
-    </div>
+    <GradientCard>
+      <div className=" flex w-[400px]  justify-between items-center  aspect-video   rounded-2xl border-2 border-borderColor">
+        <div className="flex flex-col justify-center px-5">
+          <h1 className="font-bold text-2xl text-center">{dataNumber}</h1>
+          <h2 className="text-gray-500 relative top-5 text-center">{title}</h2>
+        </div>
+        <Image src={image || img} alt=""></Image>
+      </div>
+    </GradientCard>
   );
 }
