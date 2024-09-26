733be7a62ec2de9fee82cf436f68fcc4f8cdf5a5
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-09-26 19:33:12 +0530

    🐛 Bug fix url fix

--- app/student/layout.tsx
+++ app/student/layout.tsx
@@ -14,7 +14,7 @@ export default function AdminPanelLayout({
       title: "Student",
       items: [
         {
-          href: "/admin/dashboard",
+          href: "/student",
           label: "Dashboard",
           icon: (
             <svg
@@ -32,7 +32,7 @@ export default function AdminPanelLayout({
           ),
         },
         {
-          href: "/admin/dashboard/students",
+          href: "/student/joinclass",
           label: "Join Class",
           icon: (
             <svg
@@ -52,7 +52,7 @@ export default function AdminPanelLayout({
           ),
         },
         {
-          href: "/admin/dashboard/teachers",
+          href: "/student/worksheet",
           label: "Worksheets",
           icon: (
             <svg
@@ -69,29 +69,29 @@ export default function AdminPanelLayout({
             </svg>
           ),
         },
+        // {
+        //   href: "/student/Announcement",
+        //   label: "Annoucement",
+        //   icon: (
+        //     <svg
+        //       xmlns="http://www.w3.org/2000/svg"
+        //       width="20"
+        //       height="20"
+        //       viewBox="0 0 24 24"
+        //       fill="none"
+        //       stroke="currentColor"
+        //       stroke-width="2"
+        //       stroke-linecap="round"
+        //       stroke-linejoin="round"
+        //       className="lucide lucide-bell"
+        //     >
+        //       <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
+        //       <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
+        //     </svg>
+        //   ),
+        // },
         {
-          href: "/admin/dashboard/teachers",
-          label: "Annoucement",
-          icon: (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-bell"
-            >
-              <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
-              <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
-            </svg>
-          ),
-        },
-        {
-          href: "/admin/dashboard/teachers",
+          href: "/",
           label: "Sign Out",
           icon: (
             <svg
