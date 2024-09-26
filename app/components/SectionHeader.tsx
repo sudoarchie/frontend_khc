79462be4de6e4324affcf6d982e1a56127640a5c
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-09-26 18:57:58 +0530

    🐛 Bug fix section header auto reload after url change using pathname hook in next/navigation

--- app/components/SectionHeader.tsx
+++ app/components/SectionHeader.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export function SectionHeader() {
   const [section, setSection] = useState<string>("");
+  const pathname = usePathname();
   useEffect(() => {
     const urlExtract = () => {
       const currentUrl = window.location.href;
@@ -12,7 +14,7 @@ export function SectionHeader() {
     };
 
     urlExtract();
-  }, []);
+  }, [pathname]);
 
   return <h2 className="text-2xl m-2 font-bold">{section}</h2>;
 }
