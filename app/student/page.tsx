31ad2172ab707c5d1de2a6e43e9cda24b3215083
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-10-31 16:13:22 +0530

    student dashboard api integration

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Breadcrumb } from "@/app/components/Breadcrumbs";
 import { DashboardCard } from "@/app/components/DashboardCard";
 import { NavbarSec } from "@/app/components/NavbarSecoundary";
@@ -8,25 +9,63 @@ import img1 from "@/public/Frame 97 (2).png";
 import img2 from "@/public/Frame 97.png";
 import { VideoCard } from "../components/videoCard";
 import { PrimaryButton } from "../components/PrimaryButton";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Loader from "../components/Loading";
+import { StaticImageData } from "next/image";
+import { Key } from "react";
+import Link from "next/link";
 
 export default function Page() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: [`DashboardData`],
+    queryFn: async () => {
+      const data = await axios({
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/student/dashboarddata`,
+        method: "get",
+        withCredentials: true,
+      });
+
+      return data.data.data;
+    },
+  });
+  const videoData = useQuery({
+    queryKey: [`generalvideo`],
+    queryFn: async () => {
+      const data = await axios({
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/video?take=15&skip=0`,
+        method: "get",
+        withCredentials: true,
+      });
+      return data.data.data;
+    },
+  });
+  console.log(videoData.data);
+  if (isLoading) {
+    return <Loader></Loader>;
+  }
+  if (error) {
+    return <div> error</div>;
+  }
+  console.log(data);
   const dashboardData = [
     {
-      title: "Total Number of Student",
-      data: 3423,
+      title: "Course Enrolled",
+      data: data.enrollment,
       image: img2,
     },
     {
-      title: "Total Number of Teacher",
-      data: 423,
+      title: "Number of Assignment",
+      data: data.Assignment,
       image: img1,
     },
     {
-      title: "Total Number of Teacher",
-      data: 423,
+      title: "Average Assignment Marks",
+      data: data.testmarks || 0,
       image: img,
     },
   ];
+
   const VideoData = [
     {
       title: "Terraform Course - Automate your AWS cloud infrastructure",
@@ -66,7 +105,6 @@ export default function Page() {
     },
   ];
 
-  const data = [];
   return (
     <div className="w-full flex flex-wrap gap-5 m-14">
       {dashboardData.map((info, index) => (
@@ -77,23 +115,36 @@ export default function Page() {
           image={info.image}
         ></DashboardCard>
       ))}
-      <div className="mt-5">
+      <div className="mt-5 w-full">
         <h1 className="text-3xl font-semibold">Latest Uploads</h1>
-        <h2 className="text-gray-600 mb-5">All latest video uploaded </h2>
-        <div className="flex flex-wrap justify-center items-center gap-5">
-          {VideoData.map((info, index) => (
-            <VideoCard
-              title={info.title}
-              href={info.href}
-              image={info.image}
-              key={index}
-            ></VideoCard>
-          ))}
+        <h2 className="text-gray-600 mb-5 w-full">
+          All latest video uploaded{" "}
+        </h2>
+        <div className="flex flex-wrap  items-center gap-5">
+          {videoData.data.map(
+            (
+              info: {
+                name: string;
+                url: string | undefined;
+                image: string | StaticImageData | undefined;
+              },
+              index: Key | null | undefined
+            ) => (
+              <VideoCard
+                title={info.name}
+                href={info.url}
+                image={info.image}
+                key={index}
+              ></VideoCard>
+            )
+          )}
         </div>
-        <PrimaryButton
-          Name={"Watch More"}
-          className="mx-auto mt-5"
-        ></PrimaryButton>
+        <Link href={"../videos"}>
+          <PrimaryButton
+            Name={"Watch More"}
+            className="mx-auto mt-5"
+          ></PrimaryButton>
+        </Link>
       </div>
     </div>
   );
