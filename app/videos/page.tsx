d6fc6cc858875e32da30c220a9ba6b0b374c9b22
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-11-03 16:59:21 +0530

    ✨ New feature /video api integration

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -1,62 +1,38 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
 import { VideoCard } from "../components/videoCard";
-
+import axios from "axios";
+import Loader from "../components/Loading";
+interface VideoSchema {
+  id: string;
+  name: string;
+  discription: string;
+  url: string;
+}
 export default function Videos() {
-  const data = [
-    {
-      title: "How To Build Anything | Art of Reverse Engineering",
-      href: "https://youtu.be/EiOvxYja8GU",
+  const { data, isLoading, error } = useQuery<VideoSchema[]>({
+    queryKey: ["video"],
+    queryFn: async () => {
+      const response = await axios({
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/video?take=15&skip=0`,
+        method: "get",
+      });
+      console.log(response.data.data);
+      return response.data.data;
     },
-    {
-      title: "How To Build Anything | Art of Reverse Engineering",
-      href: "https://youtu.be/EiOvxYja8GU",
-    },
-    {
-      title: "How To Build Anything | Art of Reverse Engineering",
-      href: "https://youtu.be/EiOvxYja8GU",
-    },
-    {
-      title: "How To Build Anything | Art of Reverse Engineering",
-      href: "https://youtu.be/EiOvxYja8GU",
-    },
-    {
-      title: "How To Build Anything | Art of Reverse Engineering",
-      href: "https://youtu.be/EiOvxYja8GU",
-    },
-    {
-      title: "How To Build Anything | Art of Reverse Engineering",
-      href: "https://youtu.be/EiOvxYja8GU",
-    },
-    {
-      title: "How To Build Anything | Art of Reverse Engineering",
-      href: "https://youtu.be/EiOvxYja8GU",
-    },
-    {
-      title: "How To Build Anything | Art of Reverse Engineering",
-      href: "https://youtu.be/EiOvxYja8GU",
-    },
-    {
-      title: "How To Build Anything | Art of Reverse Engineering",
-      href: "https://youtu.be/EiOvxYja8GU",
-    },
-    {
-      title: "How To Build Anything | Art of Reverse Engineering",
-      href: "https://youtu.be/EiOvxYja8GU",
-    },
-    {
-      title: "How To Build Anything | Art of Reverse Engineering",
-      href: "https://youtu.be/EiOvxYja8GU",
-    },
-  ];
+  });
+  if (isLoading) {
+    return <Loader></Loader>;
+  }
+  if (error) {
+    return <div>Error</div>;
+  }
   return (
     <div>
       <h1 className="text-center font-bold text-4xl my-5">More Videos</h1>
       <div className="flex flex-wrap gap-5 justify-center w-[90%] mx-auto">
-        {data.map((info, index) => (
-          <VideoCard
-            title={info.title}
-            href={info.href}
-            key={index}
-          ></VideoCard>
+        {data?.map(({ id, name, url }) => (
+          <VideoCard title={name} href={url} key={id} />
         ))}
       </div>
     </div>
