5a88c6fc99650eea02b5f3b6257174b7cf45a82a
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-11-03 08:37:13 +0530

    🐛 Bug fix add blog input-> mui

--- app/admin/dashboard/blogs/add/page.tsx
+++ app/admin/dashboard/blogs/add/page.tsx
@@ -1,10 +1,10 @@
 import { Box } from "@/app/components/Box";
-import { InputField } from "@/app/components/Inputfield";
 import { MultiOptionSelect } from "@/app/components/MultiOptionSearch";
 import { PrimaryButton } from "@/app/components/PrimaryButton";
 import { TextAreaField } from "@/app/components/TextAreaField";
 import { UploadField } from "@/app/components/UploadField";
 import { MultiSelect } from "react-multi-select-component";
+import TextField from "@mui/material/TextField";
 
 export default function Add() {
   const option = [
@@ -14,43 +14,37 @@ export default function Add() {
     { label: "cat4", value: "cat4" },
     { label: "cat5", value: "cat5" },
   ];
-  return (
-    <div className=" w-full">
-      <Box Heading="Create a new post" className="p-5 flex flex-col gap-5">
-        <InputField
-          label="Title"
-          placeholder={"Blog Heading"}
-          type={"Text"}
-          className=""
-        ></InputField>
-        <TextAreaField
-          label={"Discription"}
-          placeholder={""}
-          rows={3}
-        ></TextAreaField>
 
+  return (
+    <div className="w-full">
+      <Box Heading="Create a new post" className="p-5 flex flex-col gap-5">
+        {/* Replaced InputField with MUI TextField */}
+        <TextField
+          label="Title"
+          placeholder="Blog Heading"
+          variant="outlined"
+          fullWidth
+          className=""
+        />
+        <TextField
+          id="outlined-multiline-static"
+          label="Description"
+          multiline
+          rows={4}
+        />{" "}
         <Box Heading="" className="w-full m-auto">
-          <TextAreaField
-            label={"Content"}
-            placeholder={""}
-            rows={10}
-          ></TextAreaField>
-          <br></br>
           <h1 className="font-bold text-xl my-5">Cover Image</h1>
-          <UploadField></UploadField>
+          <UploadField />
         </Box>
         <Box
           Heading="Properties"
           SubHeading="Additional attribute and functionality"
         >
-          <MultiOptionSelect label={"Tags"} option={option}></MultiOptionSelect>
+          <MultiOptionSelect label="Tags" option={option} />
         </Box>
-        <div className="flex gap-5 ">
-          <PrimaryButton
-            Name={"Preview"}
-            className="bg-secondary w-32"
-          ></PrimaryButton>
-          <PrimaryButton Name={"Submit"} className="w-32"></PrimaryButton>
+        <div className="flex gap-5">
+          <PrimaryButton Name="Preview" className="bg-secondary w-32" />
+          <PrimaryButton Name="Submit" className="w-32" />
         </div>
       </Box>
     </div>
