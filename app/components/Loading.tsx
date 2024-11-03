56bccef97614d834eb970f955df76a3386ee1177
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-11-03 20:18:35 +0530

    ✨ New feature Loader

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,9 +1,10 @@
 export default function Loader() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="relative">
-        <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
-        <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin"></div>
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="flex-col gap-4 w-full flex items-center justify-center">
+        <div className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full">
+          <div className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full" />
+        </div>
       </div>
     </div>
   );
