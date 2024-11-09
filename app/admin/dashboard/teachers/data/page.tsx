d4a8723d5c92cb49ac66869c99c285ea0c9f0dfb
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-11-09 23:27:55 +0530

    teacher data get

--- app/admin/dashboard/teachers/data/page.tsx
+++ app/admin/dashboard/teachers/data/page.tsx
@@ -1,56 +1,42 @@
+"use client";
 import TableField from "@/app/components/TableField";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Teacher() {
-  const data = [
-    {
-      TeacherID: "T001",
-      Name: "John Doe",
-      Email: "johndoe@example.com",
-      PhoneNo: "+1-555-123-4567",
-      JoinDate: "2023-01-15",
-      ViewProfile: "https://example.com/teachers/johndoe",
-      DisplayName: "Mr. John",
+  const [table, setTable] = useState([]);
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["TeacherData"],
+    queryFn: async () => {
+      const response = await axios({
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/teacher/all`,
+        method: "get",
+        params: {
+          take: 30,
+        },
+        withCredentials: true,
+      });
+      console.log(response.data.data);
+
+      // Flatten each entry to include only necessary fields directly
+      const formattedData = response.data.data.map((info: any) => ({
+        id: info.id,
+        name: info.name,
+        email: info.email,
+        mobileNo: info.mobileNo,
+        profileLink: `${process.env.NEXT_PUBLIC_BASE_URL}/teacher/${info.id}`,
+      }));
+
+      setTable(formattedData);
+
+      return response.data.data;
     },
-    {
-      TeacherID: "T002",
-      Name: "Jane Smith",
-      Email: "janesmith@example.com",
-      PhoneNo: "+1-555-234-5678",
-      JoinDate: "2022-08-20",
-      ViewProfile: "https://example.com/teachers/janesmith",
-      DisplayName: "Ms. Smith",
-    },
-    {
-      TeacherID: "T003",
-      Name: "Robert Brown",
-      Email: "robertbrown@example.com",
-      PhoneNo: "+1-555-345-6789",
-      JoinDate: "2021-09-30",
-      ViewProfile: "https://example.com/teachers/robertbrown",
-      DisplayName: "Mr. Brown",
-    },
-    {
-      TeacherID: "T004",
-      Name: "Emily Davis",
-      Email: "emilydavis@example.com",
-      PhoneNo: "+1-555-456-7890",
-      JoinDate: "2023-03-12",
-      ViewProfile: "https://example.com/teachers/emilydavis",
-      DisplayName: "Ms. Emily",
-    },
-    {
-      TeacherID: "T005",
-      Name: "Michael Johnson",
-      Email: "michaeljohnson@example.com",
-      PhoneNo: "+1-555-567-8901",
-      JoinDate: "2020-11-05",
-      ViewProfile: "https://example.com/teachers/michaeljohnson",
-      DisplayName: "Mr. Johnson",
-    },
-  ];
+  });
+
   return (
     <div>
-      <TableField rows={data} className="mt-5"></TableField>
+      <TableField rows={table} className="mt-5" />
     </div>
   );
 }
