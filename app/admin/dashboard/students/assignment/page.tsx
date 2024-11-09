0e0e0e0a943b58e6c3e240f9a2db68523422f30a
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-11-09 23:27:39 +0530

    Assignement get for admin

--- app/admin/dashboard/students/assignment/page.tsx
+++ app/admin/dashboard/students/assignment/page.tsx
@@ -1,51 +1,26 @@
+"use client";
 import TableField from "@/app/components/TableField";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import Link from "next/link";
 
 export default function Assignment() {
-  const data = [
-    {
-      "Document Name": "Introduction to Java",
-      "Document Type": "PDF",
-      Type: "Lecture Notes",
-      Chapter: "Chapter 1",
-      "Shared Date": "2024-01-10",
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["Assignment"],
+    queryFn: async () => {
+      const response = await axios({
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/assignment/all`,
+        method: "get",
+        withCredentials: true,
+        params: {
+          take: 30,
+        },
+      });
+      console.log(response.data.data);
+      return response.data.data;
     },
-    {
-      "Document Name": "Data Structures Overview",
-      "Document Type": "DOCX",
-      Type: "Assignment",
-      Chapter: "Chapter 3",
-      "Shared Date": "2024-02-15",
-    },
-    {
-      "Document Name": "Operating Systems Basics",
-      "Document Type": "PPT",
-      Type: "Lecture Slides",
-      Chapter: "Chapter 2",
-      "Shared Date": "2024-03-05",
-    },
-    {
-      "Document Name": "Networking Fundamentals",
-      "Document Type": "PDF",
-      Type: "Lab Manual",
-      Chapter: "Chapter 4",
-      "Shared Date": "2024-04-01",
-    },
-    {
-      "Document Name": "Database Management Systems",
-      "Document Type": "PDF",
-      Type: "Lecture Notes",
-      Chapter: "Chapter 5",
-      "Shared Date": "2024-05-12",
-    },
-    {
-      "Document Name": "Software Engineering Concepts",
-      "Document Type": "DOCX",
-      Type: "Project Document",
-      Chapter: "Chapter 6",
-      "Shared Date": "2024-06-20",
-    },
-  ];
+  });
+
   return (
     <div>
       <TableField className="my-5" rows={data}></TableField>
