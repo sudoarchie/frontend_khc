73270f668d3f2a1c0e0f7600567c5aa27d957cc4
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-11-24 13:50:42 +0530

    Image change

--- app/admin/dashboard/management/page.tsx
+++ app/admin/dashboard/management/page.tsx
@@ -1,19 +1,19 @@
 import { CardTwo } from "@/app/components/cardTwo";
+import img1 from "@/public/7.png";
 
+import img2 from "@/public/8.png";
 export default function Management() {
   return (
     <div className="flex gap-5 p-5 w-[95%]">
       <CardTwo
         title={"Subject Management"}
+        image={img1}
         url={"/admin/dashboard/management/subjectmanagement"}
       ></CardTwo>
       <CardTwo
         title={"Category Management"}
+        image={img2}
         url={"/admin/dashboard/management/categorymanagement"}
-      ></CardTwo>
-      <CardTwo
-        title={"Material Management"}
-        url={"/admin/dashboard/management/materialmanagement"}
       ></CardTwo>
     </div>
   );
