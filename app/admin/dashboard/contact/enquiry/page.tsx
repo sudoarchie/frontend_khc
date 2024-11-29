472ca4375b2d0996cf23a13949300edeb4b6c270
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-11-29 21:37:25 +0530

    contact info

--- app/admin/dashboard/contact/enquiry/page.tsx
+++ app/admin/dashboard/contact/enquiry/page.tsx
@@ -1,54 +1,51 @@
+"use client";
+import { Error } from "@/app/components/error";
+import Loader from "@/app/components/Loading";
 import TableField from "@/app/components/TableField";
-
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+interface Data {
+  email: string;
+  mobileNo: string;
+  message: string;
+}
 export default function Enquiry() {
-  const data = [
-    {
-      "subject": "Mathematics",
-      "studentName": "Alice Johnson",
-      "grade": "A",
-      "country": "USA",
-      "phoneNo": "+1-202-555-0123",
-      "email": "alice.johnson@example.com",
-      "time": "2024-09-05T14:23:00Z"
+  const { data, isLoading, error } = useQuery<Data[]>({
+    queryKey: ["enquire"],
+    queryFn: async () => {
+      const response = await axios({
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/enquiry/`,
+        method: "GET",
+        params: {
+          take: 30,
+        },
+      });
+      return response.data.data;
     },
-    {
-      "subject": "Physics",
-      "studentName": "David Smith",
-      "grade": "B",
-      "country": "Canada",
-      "phoneNo": "+1-416-555-0145",
-      "email": "david.smith@example.com",
-      "time": "2024-09-05T10:45:00Z"
-    },
-    {
-      "subject": "History",
-      "studentName": "Maria Garcia",
-      "grade": "A+",
-      "country": "Spain",
-      "phoneNo": "+34-910-555-0189",
-      "email": "maria.garcia@example.com",
-      "time": "2024-09-04T16:15:00Z"
-    },
-    {
-      "subject": "Chemistry",
-      "studentName": "Chen Wei",
-      "grade": "B+",
-      "country": "China",
-      "phoneNo": "+86-10-5555-0123",
-      "email": "chen.wei@example.cn",
-      "time": "2024-09-04T08:30:00Z"
-    },
-    {
-      "subject": "English Literature",
-      "studentName": "John Doe",
-      "grade": "C",
-      "country": "UK",
-      "phoneNo": "+44-20-5555-0198",
-      "email": "john.doe@example.co.uk",
-      "time": "2024-09-05T09:50:00Z"
-    }
-  ]
-  return (<div className="m-5">
-    <TableField rows={data} deleteOption={true}></TableField>
-  </div>);
+  });
+  if (isLoading) return <Loader></Loader>;
+  if (error) return <Error></Error>;
+
+  return (
+    <div className="m-5">
+      <table className="w-full">
+        <thead className="bg-blue-600 text-white  h-16">
+          <tr>
+            <th>Email</th>
+            <th>Mobile No</th>
+            <th>Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((item: Data) => (
+            <tr className="border-b border-gray-200 text-center h-16">
+              <td>{item.email}</td>
+              <td>{item.mobileNo}</td>
+              <td className="text-wrap">{item.message}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
--- app/admin/dashboard/contact/page.tsx
+++ app/admin/dashboard/contact/page.tsx
@@ -1,13 +1,20 @@
 import { CardTwo } from "@/app/components/cardTwo";
+import img1 from "@/public/10.png";
+import img2 from "@/public/11.png";
 
 export default function Contact() {
   return (
     <div className="flex gap-5 flex-wrap m-5">
       <CardTwo
+        image={img1}
         title={"Free Class Enquiry"}
         url={"/admin/dashboard/contact/enquiry"}
       ></CardTwo>
-      <CardTwo title={"Inbox"} url={"/admin/dashboard/contact/inbox"}></CardTwo>
+      {/* <CardTwo
+        image={img2}
+        title={"Inbox"}
+        url={"/admin/dashboard/contact/inbox"}
+      ></CardTwo> */}
     </div>
   );
 }
